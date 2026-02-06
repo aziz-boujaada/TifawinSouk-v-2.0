1679ec0d0400f83f5bf619dbb05ea84bac7f211d
aziz-boujaada <azizboujaada87@gmail.com>
2026-02-06 16:40:20 +0100

    .

---
+++ Tifawin-Souk-V2/app/Http/Controllers/TestProductController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Cart;
+use App\Models\CartItem;
+use App\Models\Order;
+use App\Models\OrderItem;
+use App\Models\Product;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class TestProductController extends Controller
+{
+  public function index()
+  {
+    $products = Product::all();
+    return view('test', compact('products'));
+  }
+
+  public function cart()
+  {
+    $cartItems = CartItem::with('product')->get();
+    return view('cart-ui', compact('cartItems'));
+  }
+
+
+}
