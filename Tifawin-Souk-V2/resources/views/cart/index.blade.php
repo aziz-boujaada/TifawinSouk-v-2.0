4c808dbe873dde1ada64a858048d0ea2ed9339e4
zahrae028 <fatimabk028@gmail.com>
2026-02-09 17:06:22 +0100

    feat: enhance cart page layout and styling with new components

--- Tifawin-Souk-V2/resources/views/cart/index.blade.php
+++ Tifawin-Souk-V2/resources/views/cart/index.blade.php
@@ -1,53 +1,28 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <script src="https://cdn.tailwindcss.com"></script>
+<html>
+    @include('components.head')
 
-    <title>Document</title>
-</head>
-<body>
-    
-    <div class="container mx-auto p-4">
+    <body>
+         @include('components.header')
+        
+    <div class="cart-infos">
+        <p class="cart-info">Total number of items ({{ $cart->totalQuantity() }})</p>
+        <p class="cart-info">Totale price {{ $cart->totalPrice() }} MAD</p>
+    </div>
+        <section class="cart-section">
+        @forelse($cart->items as $cartItem)
+                       
+                <div class="product-container">
 
-        <table class="min-w-full bg-white border border-gray-200 mb-6">
-            <thead>
-                <tr class="bg-gray-100">
-                    <th class="py-2 px-4 border-b">ID</th>
-                    <th class="py-2 px-4 border-b">USER</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td class="py-2 px-4 border-b">{{ $cart->id }}</td>
-                    <td class="py-2 px-4 border-b">{{ $cart->user->name }}</td>
-                </tr>
-            </tbody>
-        </table>
-    
-        <form action="{{ route('save-order-items') }}" method="post">
-            @csrf
-    
-            <table class="min-w-full bg-white border border-gray-200 mb-4">
-                <thead>
-                    <tr class="bg-gray-100">
-                        <th class="py-2 px-4 border-b">Cart ID</th>
-                        <th class="py-2 px-4 border-b">Product ID</th>
-                        <th class="py-2 px-4 border-b">Quantity</th>
-                        <th class="py-2 px-4 border-b">Product Name</th>
-                        <th class="py-2 px-4 border-b">Price</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @forelse($cart->items as $cartItem)
-                        <tr>
-                            <td class="py-2 px-4 border-b">{{ $cartItem->cart_id }}</td>
-                            <td class="py-2 px-4 border-b">{{ $cartItem->product_id }}</td>
-                            <td class="py-2 px-4 border-b">{{ $cartItem->quantity }}</td>
-                            <td class="py-2 px-4 border-b">{{ $cartItem->product->name }}</td>
-                            <td class="py-2 px-4 border-b">{{ $cartItem->product->price }}</td>
-                        </tr>
+                <img src="{{ $cartItem->product->image }}" alt="{{ $cartItem->product->reference }}">
+                <h2>{{ $cartItem->product->name }}</h2>
+                <p>{{ $cartItem->product->description }}</p>
+                <p class="price">{{ $cartItem->product->price }} MAD</p>
+                <p class="quantity">{{ $cartItem->quantity }}</p>
+                </div>
+                            <!-- <td class="">{{ $cartItem->product->name }}</td>
+                            <td class="">{{ $cartItem->product->price }}</td>
+                      -->
     
                    
                         <input type="hidden" name="items[{{ $loop->index }}][product_id]" value="{{ $cartItem->product_id }}">
@@ -60,22 +35,7 @@
                             </td>
                         </tr>
                     @endforelse
-                </tbody>
-            </table>
-    
-            @if($cart->items->count())
-                <button type="submit" class="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">
-                    Order
-                </button>
-            @endif
-        </form>
-    
-   
-        <div class="mt-4 space-x-4">
-            <span class="font-semibold">Total Quantity:</span> <span>{{ $cart->totalQuantity() }}</span>
-            <span class="font-semibold">Total Price:</span> <span>{{ $cart->totalPrice() }}</span>
-        </div>
-    
-    </div>
-</body>
+        </section>
+        <button class="checkout btn btn-primary">Checkout</button>
+    </body>
 </html>
