e0f76f700c07535522992a78ee7a6595bde3af11
zahrae028 <fatimabk028@gmail.com>
2026-02-10 17:01:41 +0100

    feat: refactor header navigation to use consistent class naming and styling

--- Tifawin-Souk-V2/resources/views/components/header.blade.php
+++ Tifawin-Souk-V2/resources/views/components/header.blade.php
@@ -1,13 +1,13 @@
 <header>
-                <nav >
-                <div>
-                    <a class="logo" href="">Tifawin</a>
-                </div>
-                <div class="nav-bar">
-                    <a href="">Cart</a>
-                    <a href="">Orders</a>
-                    <a href="">Profile</a>
-                </div>
+                <nav class="header">
+                    <div>
+                        <a class="logo" href="">Tifawin</a>
+                    </div>
+                    <div class="nav-bar">
+                        <a href="">Cart</a>
+                        <a href="">Orders</a>
+                        <a href="">Profile</a>
+                    </div>
 
                 </nav>
 </header>
